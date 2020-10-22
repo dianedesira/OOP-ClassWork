041b613e9e4fcef2f4f4bbbd939660d74aa504c0
Diane Desira <diane.borg@mcast.edu.mt>
2020-10-22 14:26:01 +0200

    Created CarScenario project

---
+++ CarScenario/CarScenario/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarScenario
+{
+    class Car
+    {
+        string manufacturer;
+        string colour;
+        int odoMeterReading;
+
+        public void Drive()
+        {
+            
+        }
+        
+        public void RePaint()
+        {
+            
+        }
+
+        public void FillWithGas()
+        {
+            
+        }
+    }
+}
